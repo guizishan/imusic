118d4d1f320672d72e170c6fb51a976808d7d752
guizishan <1023569191@qq.com>
2017-11-21 18:02:38 +0800

    'update'

--- router/user.js
+++ router/user.js
@@ -17,5 +17,27 @@ router.post('/add',async function(ctx,next){
 	// 给客户端返回响应
 	await ctx.render('success',{data:'保存成功'});
 });
+// 请求登录的表单
+router.get('/login',async function(ctx,next){
+	await ctx.render('login');
+})
+// 请求验证登录
+router.post('/login',async function(ctx,next){
+	let user = ctx.request.body;
+	//将这个user对象保存到redis
+	let db = new userDb();
+	// 等到db.validateUser方法验证的结果出来后，才能给用户响应
+	var validKey = await db.validateUser(user);
+	console.log(validKey);
+	// 给客户端返回响应
+	if(validKey){
+		await ctx.render('index',{
+			user
+		});
+	}else{
+		await ctx.redirect('/user/login');
+	}
+	
+});
 
 module.exports=router;//将这个对象暴露出去
